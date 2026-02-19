345d7ef6a8b8286c8d436161244e53b1d276e510
jnnjh <joannejhoieacar@gmail.com>
2026-02-19 13:45:39 +0100

    finished exercise 3

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,21 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+
+console.log("QTY     ITEM                TOTAL");
+
+let totalPence = 0;
+
+order.forEach(({ itemName, quantity, unitPricePence }) => {
+  const itemTotalPence = quantity * unitPricePence;
+  totalPence += itemTotalPence;
+
+  const itemTotalPounds = (itemTotalPence / 100).toFixed(2);
+
+  console.log(
+    `${quantity.toString().padEnd(8)}${itemName.padEnd(20)}${itemTotalPounds}`
+  );
+});
+
+console.log("\nTotal: " + (totalPence / 100).toFixed(2));
